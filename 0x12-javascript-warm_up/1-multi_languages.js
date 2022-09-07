82f35964f54413a5c360ca66ff8fd1655f87aa82
robel16 <99750963+robel16@users.noreply.github.com>
2022-09-07 12:05:53 -0600

    deleted js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-let firstMessage = 'C is fun'
-let secondMessage = 'Python is cool'
-let thirdMessage = 'JavaScript is amazing'
-
-console.log(firstMessage);
-console.log(secondMessage);
-console.log(thirdMessage);
