6ea193ea0563111a253b07be9ded17d72df28467
robel16 <99750963+robel16@users.noreply.github.com>
2022-12-06 07:56:59 +0300

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" -X OPTIONS | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
